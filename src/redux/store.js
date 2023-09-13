be31abed8531c7dc5742c1e41e1a7df97914eccf
Outkarm <jkpordje@gmail.com>
2023-09-13 22:34:41 +0000

    update store with presistor function

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import gameReducer from "./reducer/gameReducer";
 
-export default configureStore({ reducer: { game: gameReducer } });
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, gameReducer);
+
+const store = configureStore({ reducer: { game: persistedReducer } });
+
+const persistor = persistStore(store);
+export { store, persistor };
