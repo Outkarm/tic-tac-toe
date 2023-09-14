d5bb76f7a7de1ca539fe3a1848c6b3820d503bff
Outkarm <jkpordje@gmail.com>
2023-09-14 23:43:36 +0000

    update buttons and styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,8 @@
 import "../assets/style/button.css";
 import { played } from "../redux/reducer/gameReducer";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 
 function Button({ value, id }) {
-  // const game = useSelector((state) => state.game);
   const dispatch = useDispatch();
 
   const handlePlayed = () => {
